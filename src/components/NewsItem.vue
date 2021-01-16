9dccda17393eab5284125d9c74c4dfa718d76566
milacerveira <milascerveira@gmail.com>
2021-01-16 14:26:39 +0000

    first commit

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <li v-for="newsItem in news" :key="newsItem.id">
+      {{ newsItem.webTitle }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["news"],
+};
+</script>
---
+++ src/components/NewsList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>The Guardian</h1>
+    <input placeholder="Search for a headline" v-model="searchTerm"></input>
+    <button @click="handleSearch">Search</button>
+    <ul>
+      <NewsItem :news="news"> </NewsItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NewsItem from "./NewsItem.vue";
+
+export default {
+  name: "NewsList",
+
+  data() {
+    return {
+      news: [],
+      searchTerm: "",
+    };
+  },
+
+  components: {
+    NewsItem,
+  },
+
+  methods: {
+    handleSearch() {
+      console.log("Button is clicked, Search term: ", this.searchTerm);
+
+      window
+        .fetch(
+          `https://content.guardianapis.com/search?q=${this.searchTerm}&format=json&api-key=test`
+        )
+        .then((response) => {
+          response.json().then((json) => {
+            this.news = json.response.results;
+          });
+        });
+    },
+  },
+
+  mounted() {
+    window
+      .fetch(
+        "https://content.guardianapis.com/search?q=covid&format=json&api-key=test"
+      )
+      .then((response) => {
+        response.json().then((json) => {
+          this.news = json.response.results;
+          console.log(this.news);
+        });
+      });
+  },
+};
+</script>
